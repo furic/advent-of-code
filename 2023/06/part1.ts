b198e0458d6341a99e48c052a66dbff2fcfcb2ce
Richard Fu <44884321+furic@users.noreply.github.com>
2024-01-02 01:16:17 +0000

    Add solution for 2023/06

---
+++ 2023/06/part1.ts
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync("input", "utf8").split("\n");
+
+let result = 1;
+
+const recordDistance = input[1].split(": ")[1].split(" ").map(x => Number(x)).filter(Number);
+const timeDistance = input[0].split(": ")[1].split(" ").map(x => Number(x)).filter(Number).map((x, i) => [x, recordDistance[i]]);
+
+for (const [time, distance] of timeDistance) {
+    let wayCount = 0;
+    for (let i = 1; i < time - 1; i++) {
+        const newDistance = (time - i) * i;
+        if (newDistance > distance) {
+            wayCount++;
+        }
+    }
+    result *= wayCount;
+}
+
+console.log(result);
---
+++ 2023/06/part2.ts
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync("input", "utf8").split("\n");
+
+let result = 0;
+
+const time = Number(input[0].split(": ")[1].replace(/ /g, ""));
+const distance = Number(input[1].split(": ")[1].replace(/ /g, ""));
+
+for (let i = 1; i < time - 1; i++) {
+    const newDistance = (time - i) * i;
+    if (newDistance > distance)
+        result++;
+}
+
+console.log(result);
