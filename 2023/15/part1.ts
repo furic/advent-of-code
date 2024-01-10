1610597d7ef47e86b6d360fb60f7c5abf65f7d07
Richard Fu <44884321+furic@users.noreply.github.com>
2024-01-10 00:37:14 +0000

    Add 2023/15 solution

---
+++ 2023/15/part1.ts
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync("input", "utf8").split(",");
+
+let result = 0;
+
+for (const str of input) {
+    result += str.split("").map(x => x.charCodeAt(0)).reduce((a, x) => 17 * (a + x) % 256, 0);
+}
+
+console.log(result);
