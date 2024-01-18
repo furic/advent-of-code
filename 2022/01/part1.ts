b9295f80d7fa425cc850ba7f1d8e477e37c4c530
Richard Fu <44884321+furic@users.noreply.github.com>
2024-01-18 00:48:51 +0000

    Add 2022/01 solution

---
+++ 2022/01/part1.ts
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync("input", "utf8").split("\n");
+
+let result = 0;
+let current = 0;
+
+for (const line of input) {
+    if (line == "") {
+        result = Math.max(result, current);
+        current = 0;
+    } else {
+        current += Number(line);
+    }
+}
+
+console.log(result);
---
+++ 2022/01/part2.ts
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync("input", "utf8").split("\n");
+
+let result = [];
+let current = 0;
+
+for (const line of input) {
+    if (line == "") {
+        result.push(current);
+        current = 0;
+    } else {
+        current += Number(line);
+    }
+}
+
+console.log(result.sort((a, b) => b - a).slice(0, 3).reduce((a, b) => a + b, 0));
