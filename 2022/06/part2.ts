0497518202a1ae6c44726ff53cd780204399586e
Richard Fu <44884321+furic@users.noreply.github.com>
2024-01-23 05:26:59 +0000

    Add 2022/06 solution

---
+++ 2022/06/part2.ts
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync("input", "utf8");
+
+const markerLength = 14;
+
+let index = 0;
+for (; index < input.length; index++) {
+    const uniqueCount = new Set(input.substring(index, index + markerLength).split("")).size;
+    if (uniqueCount === markerLength) {
+        break;
+    }
+}
+
+console.log(index + markerLength);
