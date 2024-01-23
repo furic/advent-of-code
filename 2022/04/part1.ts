bcdd7736bd7e2aac420795dd9edd90b5d2a5c7d5
Richard Fu <44884321+furic@users.noreply.github.com>
2024-01-23 03:40:18 +0000

    Add 2022/04 solution

---
+++ 2022/04/part1.ts
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync("input", "utf8").split("\n").map((x) => x.split(",").map((y) => y.split("-").map((z) => Number(z))));
+
+let result = 0;
+
+for (const sections of input) {
+    if (sections[0][0] >= sections[1][0] && sections[0][1] <= sections[1][1]) {
+        result++;
+    } else if (sections[1][0] >= sections[0][0] && sections[1][1] <= sections[0][1]) {
+        result++;
+    }
+}
+
+console.log(result);
