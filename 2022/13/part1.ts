ca56438106d2c8c18ba91eba7ef4c3e92478b7ae
Richard Fu <fur@richardfu.net>
2024-02-01 14:19:12 +1100

    Add 2022/13 solution

---
+++ 2022/13/part1.ts
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs.readFileSync("input", "utf8").split("\n\n").map(x => x.split("\n").map(y => JSON.parse(y)));
+
+const check = (a, b) => {
+    for (let i = 0; i < a.length && i < b.length; i++) {
+        if (Number.isInteger(a[i]) && Number.isInteger(b[i])) {
+            if (a[i] !== b[i]) {
+                return a[i] - b[i];
+            }
+        } else {
+            const result = check(
+                Number.isInteger(a[i]) ? [a[i]] : a[i],
+                Number.isInteger(b[i]) ? [b[i]] : b[i],
+            );
+            if (result !== 0) {
+                return result;
+            }
+        }
+    }
+    return a.length - b.length;
+};
+
+const result = input.map((pair, i) => (check(...pair) < 0 ? i + 1 : 0))
+    .reduce((a, b) => a + b);
+
+console.log(result);
---
+++ 2022/13/part2.ts
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs.readFileSync("input", "utf8").split("\n").filter(x => x).map(x => JSON.parse(x));
+
+const divider = [[[2]], [[6]]];
+
+const check = (a, b) => {
+    for (let i = 0; i < a.length && i < b.length; i++) {
+        if (Number.isInteger(a[i]) && Number.isInteger(b[i])) {
+            if (a[i] !== b[i]) {
+                return a[i] - b[i];
+            }
+        } else {
+            const result = check(
+                Number.isInteger(a[i]) ? [a[i]] : a[i],
+                Number.isInteger(b[i]) ? [b[i]] : b[i],
+            );
+            if (result !== 0) {
+                return result;
+            }
+        }
+    }
+    return a.length - b.length;
+};
+
+const list = input.concat(divider).sort((a, b) => check(a, b));
+
+const result = divider.map(x => list.indexOf(x) + 1).reduce((a, b) => a * b);
+
+console.log(result);
