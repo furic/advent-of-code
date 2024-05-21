ce290cb1a573fc6efb72553bb2ba2d8249905dba
Richard Fu <fur@richardfu.net>
2024-05-21 12:40:47 +1000

    Add 2022/24 solution

--- 2022/23/part1.ts
+++ 2022/23/part1.ts
@@ -1,25 +1,26 @@
 const fs = require("fs");
 const input = fs.readFileSync("input", "utf8").split('\n');
 
-const getOptions = (check, key): string[] => {
+const getOptions = (check: string, key: string): string[] => {
   const [x, y] = key.split(',').map(n => Number(n));
   if (check === 'N') return [x, x - 1, x + 1].map(x => `${x},${y - 1}`);
   if (check === 'S') return [x, x - 1, x + 1].map(x => `${x},${y + 1}`);
   if (check === 'W') return [y, y - 1, y + 1].map(y => `${x - 1},${y}`);
   if (check === 'E') return [y, y - 1, y + 1].map(y => `${x + 1},${y}`);
+  return [];
 }
 
-const move = (elves, proposals): void => {
+const move = (elves: Set<string>, proposals: Map<string, string[]>): void => {
   for (const key of proposals.keys()) {
     const moving = proposals.get(key);
-    if (moving.length === 1) {
+    if (moving?.length === 1) {
       elves.delete(moving[0]);
       elves.add(key);
     }
   }
 }
 
-const round = (elves, checks): void => {
+const round = (elves: Set<string>, checks: string[]): void => {
   const proposals = new Map();
   for (const key of elves.keys()) {
     const all = checks.flatMap(check => getOptions(check, key));
@@ -35,13 +36,13 @@ const round = (elves, checks): void => {
       }
     }
   }
-  checks.push(checks.shift());
+  checks.push(checks.shift() as string);
   move(elves, proposals);
 }
 
 const checks = ['N', 'S', 'W', 'E'];
 const elves = new Set<string>();
-input.forEach((line, y) =>
+input.forEach((line: string, y: number) =>
   line.split('').forEach((cell, x) => {
     if (cell === '#') {
       elves.add(`${x},${y}`);
@@ -56,6 +57,6 @@ const keys = [...elves.keys()].map(key => key.split(','));
 const xs = keys.map(key => +key[0]).sort((a, b) => b - a);
 const ys = keys.map(key => +key[1]).sort((a, b) => b - a);
 
-const result = (xs.at(0) - xs.at(-1) + 1) * (ys.at(0) - ys.at(-1) + 1) - elves.size;
+const result = (xs.at(0)! - xs.at(-1)! + 1) * (ys.at(0)! - ys.at(-1)! + 1) - elves.size;
 
 console.log(result);
--- 2022/23/part2.ts
+++ 2022/23/part2.ts
@@ -1,19 +1,20 @@
 const fs = require("fs");
 const input = fs.readFileSync("input", "utf8").split('\n');
 
-const getOptions = (check, key): string[] => {
+const getOptions = (check: string, key: string): string[] => {
   const [x, y] = key.split(',').map(n => Number(n));
   if (check === 'N') return [x, x - 1, x + 1].map(x => `${x},${y - 1}`);
   if (check === 'S') return [x, x - 1, x + 1].map(x => `${x},${y + 1}`);
   if (check === 'W') return [y, y - 1, y + 1].map(y => `${x - 1},${y}`);
   if (check === 'E') return [y, y - 1, y + 1].map(y => `${x + 1},${y}`);
+  return [];
 }
 
-const move = (elves, proposals): number => {
+const move = (elves: Set<string>, proposals: Map<string, string[]>): number => {
   let count = 0;
   for (const key of proposals.keys()) {
     const moving = proposals.get(key);
-    if (moving.length === 1) {
+    if (moving?.length === 1) {
       elves.delete(moving[0]);
       elves.add(key);
       count++;
@@ -22,7 +23,7 @@ const move = (elves, proposals): number => {
   return count;
 }
 
-const round = (elves, checks): number => {
+const round = (elves: Set<string>, checks: string[]): number => {
   const proposals = new Map();
   for (const key of elves.keys()) {
     const all = checks.flatMap(check => getOptions(check, key));
@@ -38,14 +39,14 @@ const round = (elves, checks): number => {
       }
     }
   }
-  checks.push(checks.shift());
+  checks.push(checks.shift() as string);
   return move(elves, proposals);
 }
 
 const checks = ['N', 'S', 'W', 'E'];
 const elves = new Set<string>();
-input.forEach((line, y) =>
-  line.split('').forEach((cell, x) => {
+input.forEach((line: string, y: number) =>
+  line.split('').forEach((cell: string, x: number) => {
     if (cell === '#') {
       elves.add(`${x},${y}`);
     }
