c86c492b06eba5c74ff4c19f64a6c4df9d44fef6
Richard Fu <fur@richardfu.net>
2024-11-27 11:26:25 +1100

    Add 2021/07 solution

---
+++ 2021/07/part1.ts
@@ -0,0 +1,14 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input', 'utf8').split(',').map(Number);
+const min = Math.min(...input);
+const max = Math.max(...input);
+
+const distances = [];
+for (let i = min; i <= max; i++) {
+	distances.push(input.reduce((prev, x) => prev + Math.abs(x - i), 0));
+}
+
+const result = Math.min(...distances);
+
+console.log(result);
---
+++ 2021/07/part2.ts
@@ -0,0 +1,17 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input', 'utf8').split(',').map(Number);
+const min = Math.min(...input);
+const max = Math.max(...input);
+
+const distances = [];
+for (let i = min; i <= max; i++) {
+	distances.push(input.reduce((prev, x) => {
+		const diff = Math.abs(x - i);
+		return prev + (diff * (diff + 1) / 2);
+	}, 0));
+}
+
+const result = Math.min(...distances);
+
+console.log(result);
