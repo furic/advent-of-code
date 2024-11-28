22725ff373736e96d7730ba6ce78f16840d269db
Richard Fu <fur@richardfu.net>
2024-11-29 10:39:27 +1100

    Add 2021/10 solution

---
+++ 2021/10/part1.ts
@@ -0,0 +1,19 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input', 'utf8').split('\n');
+
+const SCORE_MAP = { ")": 3, "]": 57, "}": 1197, ">": 25137 };
+
+const filterChunks = (line: string) => {
+    for (let len = 0; line.length !== len;) {
+        len = line.length;
+        line = line.replace(/\(\)|\[\]|\{\}|<>/g, '');
+    }
+    return line;
+}
+
+const getScore = (line: string) => SCORE_MAP[line.split('').find(c => ')]}>'.includes(c))] || 0;
+
+const scores = input.map(line => getScore(filterChunks(line)));
+
+console.log(scores.reduce((a, b) => a + b));
---
+++ 2021/10/part2.ts
@@ -0,0 +1,26 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input', 'utf8').split('\n');
+
+const SCORE_MAP = { "(": 1, "[": 2, "{": 3, "<": 4 };
+
+const filterChunks = (line: string) => {
+	for (let len = 0; line.length !== len;) {
+		len = line.length;
+		line = line.replace(/\(\)|\[\]|\{\}|<>/g, '');
+	}
+	return line;
+}
+
+const isIncomplete = (line: string) => [')', ']', '}', '>'].every(char => !line.includes(char));
+
+const getScore = (line: string) => {
+	const chars = line.split('').reverse();
+	return chars.reduce((total, c) => total * 5 + SCORE_MAP[c], 0);
+}
+
+const incompleteLines = input.map(line => filterChunks(line)).filter(line => isIncomplete(line));
+
+const scores = incompleteLines.map(line => getScore(line)).sort((a, b) => a - b);
+
+console.log(scores[Math.floor(scores.length / 2)]);
