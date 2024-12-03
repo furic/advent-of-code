513c685dacf84933b1e5b88f0442b5d41c856e14
Richard Fu <fur@richardfu.net>
2024-12-04 10:51:32 +1100

    Add 2024/01 solution

---
+++ 2024/01/part2.ts
@@ -0,0 +1,14 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input', 'utf8').split('\n')
+	.map(line => line.split(/\s+/).map(Number));
+
+const frequencyMap = new Map<number, number>();
+
+input.forEach(([, b]) => {
+	frequencyMap.set(b, (frequencyMap.get(b) || 0) + 1);
+});
+
+const result = input.reduce((sum, [x]) => sum + x * (frequencyMap.get(x) || 0), 0);
+
+console.log(result);
