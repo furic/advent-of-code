d7b5a857b6bd3c52afb7b101e8e1707787f7ca04
Richard Fu <fur@richardfu.net>
2024-12-04 13:54:21 +1100

    Add 2024/02 solution

---
+++ 2024/02/part1.ts
@@ -0,0 +1,15 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input', 'utf8').split('\n')
+	.map(line => line.split(' ').map(Number));
+
+const isSafe = (levels: number[]) => {
+	const steps = levels.slice(1).map((level, i) => level - levels[i]);
+	const isIncreasing = steps.every(step => step >= 1 && step <= 3);
+	const isDecreasing = steps.every(step => step >= -3 && step <= -1);
+	return isIncreasing || isDecreasing;
+}
+
+const result = input.filter(isSafe).length;
+
+console.log(result);
---
+++ 2024/02/part2.ts
@@ -0,0 +1,21 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input', 'utf8').split('\n')
+	.map(line => line.split(' ').map(Number));
+
+const isSafe = (levels: number[]) => {
+	const steps = levels.slice(1).map((level, i) => level - levels[i]);
+	const isIncreasing = steps.every(step => step >= 1 && step <= 3);
+	const isDecreasing = steps.every(step => step >= -3 && step <= -1);
+	return isIncreasing || isDecreasing;
+}
+
+const unsafeLevels = input.filter(levels => !isSafe(levels));
+
+const safeLevelCount = input.length - unsafeLevels.length;
+
+const fixedUnsafeLevelCount = unsafeLevels.filter(levels =>
+	levels.some((_, i) => isSafe(levels.slice(0, i).concat(levels.slice(i + 1))))
+).length
+
+console.log(safeLevelCount + fixedUnsafeLevelCount);
