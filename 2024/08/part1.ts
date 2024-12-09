904c663fe521e36e19fc998d4873af47d3ad536c
Richard Fu <fur@richardfu.net>
2024-12-09 12:42:57 +1100

    Add 2024/08 solution

---
+++ 2024/08/part1.ts
@@ -0,0 +1,30 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input', 'utf8').split('\n').map((line => line.split('')));
+
+const antennas = new Map<string, { x: number, y: number }[]>();
+input.forEach((row, y) => {
+	row.forEach((cell, x) => {
+		if (cell !== ".") {
+			antennas.set(cell, (antennas.get(cell) || []).concat({ x, y }));
+		}
+	});
+});
+
+const antinodes = new Set<string>();
+
+for (const points of antennas.values()) {
+	for (const origin of points) {
+		for (const from of points) {
+			if (origin !== from) {
+				const dx = origin.x - from.x;
+				const dy = origin.y - from.y;
+				if (input[origin.y + dy]?.[origin.x + dx]) {
+					antinodes.add(`${origin.x + dx},${origin.y + dy}`);
+				}
+			}
+		}
+	}
+}
+
+console.log(antinodes.size);
---
+++ 2024/08/part2.ts
@@ -0,0 +1,34 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input', 'utf8').split('\n').map((line => line.split('')));
+
+const antennas = new Map<string, { x: number, y: number }[]>();
+input.forEach((row, y) => {
+	row.forEach((cell, x) => {
+		if (cell !== ".") {
+			antennas.set(cell, (antennas.get(cell) || []).concat({ x, y }));
+		}
+	});
+});
+
+const antinodes = new Set<string>();
+
+for (const points of antennas.values()) {
+	for (const origin of points) {
+		for (const to of points) {
+			if (origin !== to) {
+				let x = origin.x;
+				let y = origin.y;
+				const dx = to.x - origin.x;
+				const dy = to.y - origin.y;
+				while (input[y + dy]?.[x + dx]) {
+					x += dx;
+					y += dy;
+					antinodes.add(`${x},${y}`);
+				}
+			}
+		}
+	}
+}
+
+console.log(antinodes.size);
