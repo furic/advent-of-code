23f5f89dcc3d07ff978193454f8cd8e5602d4b77
Richard Fu <fur@richardfu.net>
2024-12-11 17:55:04 +1100

    Add 2024/11 solution

---
+++ 2024/11/part1.ts
@@ -0,0 +1,24 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input', 'utf8').split(' ').map(Number);
+
+const evolve = () => {
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] === 0) {
+            input[i] = 1;
+        } else if (input[i].toString().length % 2 === 0) {
+            const s = input[i].toString();
+            const mid = s.length / 2;
+            input[i] = Number(s.slice(mid));
+            input.splice(i++, 0, Number(s.slice(0, mid)));
+        } else {
+            input[i] *= 2024;
+        }
+    }
+};
+
+for (let i = 0; i < 25; i++) {
+    evolve();
+}
+
+console.log(input.length);
---
+++ 2024/11/part2.ts
@@ -0,0 +1,32 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input', 'utf8').split(' ').map(Number);
+
+const evolveMap = new Map<string, number>();
+
+const evolve = (value: number) => {
+	if (value === 0) return [1];
+	if (value.toString().length % 2 === 0) {
+		const s = value.toString();
+		const mid = s.length / 2;
+		return [Number(s.slice(0, mid)), Number(s.slice(mid))];
+	}
+	return [value * 2024];
+};
+
+const getEvolvedTotal = (value: number, repeat: number) => {
+	if (repeat === 0) return 1;
+
+	const key = `${value},${repeat}`;
+	if (evolveMap.has(key)) return evolveMap.get(key);
+
+	const values = evolve(value);
+	const total = values.reduce((acc, v) => acc + getEvolvedTotal(v, repeat - 1), 0);
+
+	evolveMap.set(key, total);
+	return total;
+}
+
+const result = input.reduce((acc, value) => acc + getEvolvedTotal(value, 75), 0);
+
+console.log(result);
