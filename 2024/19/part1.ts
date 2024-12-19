8afc08f2aba3aac48b6f6678f20dacb257080af4
Richard Fu <fur@richardfu.net>
2024-12-19 19:53:17 +1100

    Add 2024/19 solution

---
+++ 2024/19/part1.ts
@@ -0,0 +1,25 @@
+import * as fs from 'fs';
+
+const [towelsRaw, patternsRaw] = fs.readFileSync('input', 'utf8').split('\n\n');
+const towels = new Set(towelsRaw.split(', '));
+const patterns = patternsRaw.split('\n');
+
+let impossibles = new Set<string>();
+
+const isPossible = (pattern: string) => {
+	if (towels.has(pattern))
+		return true;
+	if (impossibles.has(pattern) || pattern.length === 1)
+		return false;
+	for (let i = 1; i < pattern.length; i++) {
+		if (isPossible(pattern.slice(0, i)) && isPossible(pattern.slice(i))) {
+			return true;
+		}
+	}
+	impossibles.add(pattern);
+	return false;
+}
+
+const result = patterns.reduce((acc, pattern) => acc + Number(isPossible(pattern)), 0);
+
+console.log(result);
---
+++ 2024/19/part2.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs';
+
+const [towelsRaw, patternsRaw] = fs.readFileSync('input', 'utf8').split('\n\n');
+const towels = new Set(towelsRaw.split(', '));
+const patterns = patternsRaw.split('\n');
+
+let possibleCountMap = new Map<string, number>();
+
+const getPossibleCount = (pattern: string) => {
+	if (possibleCountMap.has(pattern))
+		return possibleCountMap.get(pattern);
+	if (pattern.length === 0)
+		return 1;
+
+	let count = 0;
+	for (let tower of towels) {
+		if (pattern.startsWith(tower))
+			count += getPossibleCount(pattern.slice(tower.length));
+	}
+
+	possibleCountMap.set(pattern, count);
+	return count;
+}
+
+const count = patterns.reduce((acc, pattern) => acc + getPossibleCount(pattern), 0);
+
+console.log(count);
