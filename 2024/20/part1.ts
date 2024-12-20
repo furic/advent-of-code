6081c295ec5fea9efa04a0e51866a4d2e7321dca
Richard Fu <fur@richardfu.net>
2024-12-20 22:30:12 +0800

    Add 2024/20 solution

---
+++ 2024/20/part1.ts
@@ -0,0 +1,90 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input', 'utf8').split('\n');
+
+const directions = [
+	{ x: 1, y: 0 },
+	{ x: 0, y: -1 },
+	{ x: -1, y: 0 },
+	{ x: 0, y: 1 },
+];
+const shortcutRequired = 100;
+
+let start = { x: 0, y: 0, isWall: false };
+let end = { x: 0, y: 0, isWall: false };
+let originalTime = undefined;
+
+const maze = input.map((line, row) =>
+	line.split('').map((cell, column) => {
+		if (cell === 'S') {
+			return (start = { x: column, y: row, isWall: false });
+		} else if (cell === 'E') {
+			return (end = { x: column, y: row, isWall: false });
+		} else {
+			return { x: column, y: row, isWall: cell === '#' };
+		}
+	}),
+);
+
+const race = (maze: { x: number, y: number, isWall: boolean }[][]) => {
+	const queue = [{ x: start.x, y: start.y, steps: 0 }];
+	const visited = new Map();
+	visited.set(`${start.x},${start.y}`, 0);
+
+	let time = Infinity;
+
+	while (queue.length > 0) {
+		const current = queue.shift();
+		if (current.x === end.x && current.y === end.y) {
+			time = Math.min(current.steps, time);
+			continue;
+		}
+
+		const neighbors = directions
+			.map((direction) => (maze[current.y + direction.y][current.x + direction.x]))
+			.filter((cell) => cell && !cell.isWall);
+
+		let searches = neighbors.map((neighbor) => ({
+			x: neighbor.x,
+			y: neighbor.y,
+			steps: current.steps + 1,
+		})).filter((search) => {
+			const key = `${search.x},${search.y}`;
+			if (originalTime && search.steps > originalTime - shortcutRequired) {
+				return false;
+			}
+			return (!visited.has(key) || visited.get(key) > search.steps);
+		})
+
+		queue.push(...searches);
+		searches.forEach((search) => visited.set(`${search.x},${search.y}`, search.steps));
+	}
+
+	return time;
+}
+
+originalTime = race(maze);
+
+let result = 0;
+
+for (let x = 1; x < maze[0].length - 1; x++) {
+	for (let y = 1; y < maze.length - 1; y++) {
+		if (maze[y][x].isWall) {
+			const neighbors = directions
+				.map((direction) => (maze[y + direction.y][x + direction.x]))
+				.filter((cell) => cell && !cell.isWall);
+
+			if (neighbors.length === 2 && (
+				Math.abs(neighbors[0].x - neighbors[1].x) === 2 ||
+				Math.abs(neighbors[0].y - neighbors[1].y) === 2
+			)) { // Only remove walls connecting 2 track grids in two opposite direction
+				const mazeClone = maze.map(row => row.map(grid => ({ ...grid })));
+				mazeClone[y][x].isWall = false;
+				const time = race(mazeClone);
+				if (originalTime - time >= shortcutRequired) result++;
+			}
+		}
+	}
+}
+
+console.log(result);
---
+++ 2024/20/part2.ts
@@ -0,0 +1,98 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input', 'utf8').split('\n');
+
+const directions = [
+	{ x: 1, y: 0 },
+	{ x: 0, y: -1 },
+	{ x: -1, y: 0 },
+	{ x: 0, y: 1 },
+];
+const maxCheatCount = 20;
+const shortcutRequired = 100;
+
+let start = { x: 0, y: 0, isWall: false };
+let end = { x: 0, y: 0, isWall: false };
+let originalTime = undefined;
+let originalTrack: { x: number, y: number }[] = [];
+
+const maze = input.map((line, row) =>
+	line.split('').map((cell, column) => {
+		if (cell === 'S') {
+			return (start = { x: column, y: row, isWall: false });
+		} else if (cell === 'E') {
+			return (end = { x: column, y: row, isWall: false });
+		} else {
+			return { x: column, y: row, isWall: cell === '#' };
+		}
+	}),
+);
+
+const race = (maze: { x: number, y: number, isWall: boolean }[][], start: { x: number, y: number }, end: { x: number, y: number }, stopIfOver = undefined) => {
+	const queue = [{ x: start.x, y: start.y, steps: 0, visited: [start] }];
+	const visited = new Map();
+	visited.set(`${start.x},${start.y}`, 0);
+
+	let time = Infinity;
+
+	while (queue.length > 0) {
+		const current = queue.shift();
+		if (current.x === end.x && current.y === end.y) {
+			if (!originalTime && current.steps < time) {
+				originalTrack = current.visited;
+			}
+			time = Math.min(current.steps, time);
+			continue;
+		}
+
+		const neighbors = directions
+			.map((direction) => (maze[current.y + direction.y][current.x + direction.x]))
+			.filter((cell) => cell && !cell.isWall);
+
+		let searches = neighbors.map((neighbor) => ({
+			x: neighbor.x,
+			y: neighbor.y,
+			steps: current.steps + 1,
+			visited: !originalTime ? [...current.visited, neighbor] : [],
+		})).filter((search) => {
+			const key = `${search.x},${search.y}`;
+			if (stopIfOver && visited > stopIfOver) return false;
+			return (!visited.has(key) || visited.get(key) > search.steps);
+		})
+
+		queue.push(...searches);
+		searches.forEach((search) => visited.set(`${search.x},${search.y}`, search.steps));
+	}
+
+	return time;
+}
+
+originalTime = race(maze, start, end);
+
+let result = 0;
+
+originalTrack.forEach((cheatStart, startIndex) => {
+	const visited = originalTrack.slice(0, startIndex);
+	const mazeClone = maze.map(row => row.map(grid => ({ ...grid })));
+	visited.forEach((pos) => mazeClone[pos.y][pos.x].isWall = true);
+
+	originalTrack.forEach((cheatEnd, endIndex) => {
+		if (endIndex <= startIndex) return;
+
+		const cheatCount = Math.abs(cheatStart.x - cheatEnd.x) + Math.abs(cheatStart.y - cheatEnd.y);
+		if (cheatCount <= 1 || cheatCount > maxCheatCount)
+			return;
+
+		if (originalTrack[startIndex + cheatCount] === cheatEnd) // Ignore cheat that's already on track
+			return;
+
+		const time = startIndex + cheatCount + (originalTime - endIndex);
+
+		if (originalTime - time >= shortcutRequired) {
+			result++;
+		}
+	});
+	console.log(startIndex, result);
+});
+
+console.log(result);
