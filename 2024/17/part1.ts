77c48b61a736ccfb42949afb78b038530aacc29e
Richard Fu <fur@richardfu.net>
2025-01-07 16:28:51 +1100

    Format 2024/17 solution

--- 2024/17/part1.ts
+++ 2024/17/part1.ts
@@ -7,35 +7,16 @@ for (let i = 0; i < program.length; i += 2) {
 	const instruction = program[i];
 	const literal = program[i + 1];
 	const combo = literal >= 4 ? [a, b, c][literal - 4] : literal;
-
-	switch (instruction) {
-		case 0:
-			a = Math.floor(a / (1 << combo));
-			break;
-		case 1:
-			b = b ^ literal;
-			break;
-		case 2:
-			b = combo % 8;
-			break;
-		case 3:
-			if (a !== 0) {
-				i = literal - 2;
-			}
-			break;
-		case 4:
-			b = b ^ c;
-			break;
-		case 5:
-			output.push(combo & 7);
-			break;
-		case 6:
-			b = Math.floor(a / (1 << combo));
-			break;
-		case 7:
-			c = Math.floor(a / (1 << combo));
-			break;
-	}
+	[
+		() => (a = Math.floor(a / (1 << combo))),
+		() => (b = b ^ literal),
+		() => (b = combo & 7),
+		() => (i = a !== 0 ? (i = literal - 2) : i),
+		() => (b = b ^ c),
+		() => output.push(combo & 7),
+		() => (b = Math.floor(a / (1 << combo))),
+		() => (c = Math.floor(a / (1 << combo))),
+	][instruction]();
 }
 
 console.log(output.join(','));
--- 2024/17/part2.ts
+++ 2024/17/part2.ts
@@ -8,35 +8,16 @@ const run = () => {
 		const instruction = program[i];
 		const literal = program[i + 1];
 		const combo = literal >= 4 ? [a, b, c][literal - 4] : literal;
-
-		switch (instruction) {
-			case 0:
-				a = Math.floor(a / (1 << combo));
-				break;
-			case 1:
-				b = b ^ literal;
-				break;
-			case 2:
-				b = combo & 7;
-				break;
-			case 3:
-				if (a !== 0) {
-					i = literal - 2;
-				}
-				break;
-			case 4:
-				b = b ^ c;
-				break;
-			case 5:
-				output.push(combo & 7);
-				break;
-			case 6:
-				b = Math.floor(a / (1 << combo));
-				break;
-			case 7:
-				c = Math.floor(a / (1 << combo));
-				break;
-		}
+		[
+			() => (a = Math.floor(a / (1 << combo))),
+			() => (b = b ^ literal),
+			() => (b = combo & 7),
+			() => (i = a !== 0 ? (i = literal - 2) : i),
+			() => (b = b ^ c),
+			() => output.push(combo & 7),
+			() => (b = Math.floor(a / (1 << combo))),
+			() => (c = Math.floor(a / (1 << combo))),
+		][instruction]();
 	}
 	return output;
 };
